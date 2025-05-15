ce7266bcfb583993197a862047d18f7ac5c5efa6
Moligouan <139350006+Moligouan@users.noreply.github.com>
2025-05-15 18:37:23 +0200

    Correction Front

--- TrashCoach/web/src/main/java/com/example/web/client/UserClient.java
+++ TrashCoach/web/src/main/java/com/example/web/client/UserClient.java
@@ -1,9 +1,13 @@
 package com.example.web.client;
 
+import com.example.web.model.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @FeignClient(name = "back", url = "http://localhost:8090")
 public interface UserClient {
--- TrashCoach/web/src/main/java/com/example/web/controller/WebController.java
+++ TrashCoach/web/src/main/java/com/example/web/controller/WebController.java
@@ -1,11 +1,10 @@
 package com.example.web.controller;
 
+import com.example.web.model.User;
 import com.example.web.service.WebService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Comparator;
@@ -20,36 +19,17 @@ public class WebController {
         this.webService = webService;
     }
 
-<<<<<<< HEAD
-   // @GetMapping("/")
-  //  public ModelAndView test(Model model) {
-=======
     @GetMapping("/")
     public ModelAndView test(Model model) {
->>>>>>> main
+
 //        [Model] [nom] = webService.[fonction()];
 //        model.addAttribute("[nom]", [nom]);
-
-//        webService.test();
-<<<<<<< HEAD
-   //     return new ModelAndView("[nom du fichier html]");
-//        return new ModelAndView("[nom du fichier html]", "[nom formulaire]", new [nom model formulaire]());
- //   }
-=======
         return new ModelAndView("[nom du fichier html]");
 //        return new ModelAndView("[nom du fichier html]", "[nom formulaire]", new [nom model formulaire]());
     }
->>>>>>> main
 
-//    @PostMapping("/")
-//    public String test2(Model model, @ModelAttribute("[nom formulaire]") [nom model formulaire] [nom])
-//    {
-////        Ta Fonction
-//        return "";
-//    }
-<<<<<<< HEAD
 @GetMapping("/")
-public ModelandView showHomePage(Model model) {
+public ModelAndView showHomePage(Model model) {
     List<User> trashList = webService.getAllTrash();
     model.addAttribute("trashList", trashList);
     return new ModelAndView("index", "trashForm", new User());
@@ -70,16 +50,13 @@ public void deleteTrash(@PathVariable Long id) {
 @GetMapping("/stats")
 public Object getTrashStats() {
     List<User> trashList = webService.getAllTrash();
-    long positiveCount = trashList.stream().filter(t-> !t.isNegative()).count()
+    long positiveCount = trashList.stream().filter(t-> !t.isNegative()).count();
     long negativeCount = trashList.stream().filter(User::isNegative).count();
 
     return new Object() {
         public final long positive = positiveCount;
         public final long negative = negativeCount;
-    }
+    };
 }
 
 }
-=======
-}
->>>>>>> main
